edb1f0987c14b26dce5a0007f20de8fe9685235b
Adam Cahan <adam.cahan@gmail.com>
2021-05-06 16:48:14 -0500

    Update top-bar.component.ts

--- src/app/top-bar/top-bar.component.ts
+++ src/app/top-bar/top-bar.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+/**
+Here we link up the different parts of our component.
+Template kind of like the view.
+`./` means that we look for the file starting from our files current location
+*/ 
 @Component({
   selector: 'app-top-bar',
   templateUrl: './top-bar.component.html',
